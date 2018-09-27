ca25e738075c29adfd73cdcbad81616b002026e6
Vodalok <visualcshape@gmail.com>
2018-09-27 19:56:15 +0800

    fix: missing space before ]

--- check.sh
+++ check.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
 # change first argument to change the interface you want to check
 node ./index.js en0
-if [ $? -eq 1]
+if [ $? -eq 1] 
 then
     echo "Number of IPs changed"
     # Insert the command you want to execute here
     # example
     pm2 reload all
-elif [ $? -eq -1]
+elif [ $? -eq -1 ]
 then
     echo "There was an error. Check logs to get detail"
-elif [$? -eq 0]
+elif [$? -eq 0 ]
 then
     echo "Number of IPs does not changed or this is the first time checking"
 fi
--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const INTERFACE_NAME = process.argv[2];
 const ipv4Count = os.networkInterfaces()[INTERFACE_NAME].filter(aIP=>aIP.family === 'IPv4').length;
 
 fs.readFile(IP_COUNT_FILENAME, 'utf8', (err,data)=>{
-    if(err.code === 'ENOENT'){
+    if(err && err.code === 'ENOENT'){
         writeNumberOfIP(Number.parseInt(ipv4Count), err=>{
             err?process.exit(-1):process.exit(0);
         });
