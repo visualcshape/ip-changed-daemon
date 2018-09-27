67ede767e83245633ea7e25fb606917ce25073be
Vodalok <visualcshape@gmail.com>
2018-09-27 19:45:55 +0800

    feat: basic functions done

---
+++ README.md
@@ -0,0 +1,14 @@
+# IP CHANGED DAEMON
+Check the number of IPs that bind to an interface is chaged or not and execute a command if there is a change. To check periodically ,you may want to use `crontab`.
+
+# Requirements
+1. `Node` has permission to write in this folder.
+
+# Usage
+To change watching network interface, edit `eth0`
+```
+node ./index.js eth0
+```
+in `check.sh` to any interface name you want.
+
+To execute the command after number of IP changed, there is a guide in `check.sh`, please check out.
---
+++ index.js
@@ -0,0 +1,33 @@
+const os = require('os');
+
+
+const os = require('os');
+const fs = require('fs');
+
+const IP_COUNT_FILENAME = 'IP_C_';
+const INTERFACE_NAME = process.argv[2];
+
+const ipv4Count = os.networkInterfaces()[INTERFACE_NAME].filter(aIP=>aIP.family === 'IPv4').length;
+
+fs.readFile(IP_COUNT_FILENAME, 'utf8', (err,data)=>{
+    if(err.code === 'ENOENT'){
+        writeNumberOfIP(Number.parseInt(ipv4Count), err=>{
+            err?process.exit(-1):process.exit(0);
+        });
+    }else{
+        const originalNumber = Number.parseInt(data);
+        writeNumberOfIP(ipv4Count, err=>{
+            if(err){
+                process.exit(-1);
+            }else{
+                (originalNumber !== ipv4Count)?process.exit(1):process.exit(0);
+            }
+        });
+    }
+});
+
+function writeNumberOfIP(numberOfIP, callback){
+    fs.writeFile(IP_COUNT_FILENAME, numberOfIP, {encoding: 'utf8'}, err=>{
+        callback(err);
+    });
+}
